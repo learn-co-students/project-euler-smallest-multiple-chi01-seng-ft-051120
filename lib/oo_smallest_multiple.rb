890124ca038beb47cdff530c39d4bdfd88606d51
jequezinho <jroverby92@gmail.com>
2020-09-09 13:26:19 -0500

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,25 @@
-# Implement your object-oriented solution here!
+class SmallestMultiple
+    def initialize(n)
+        @n = n
+    end
+
+    def lcm
+        (1..@n).inject(1) { |rez, i| self.pre_lcm(rez, i)}
+    end
+    
+    def gcd(a, b)
+        while a != b
+            if a > b
+                a -= b
+            else
+                b -= a
+            end
+        end
+        a
+    end
+    
+    def pre_lcm(a, b)
+        (a * b) / self.gcd(a, b)
+    end
+
+end
